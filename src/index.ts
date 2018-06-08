db3786ae6fd29e0d0aa7e18e032c847a191b4a15
gosho-kazuya <ketsume0211@gmail.com>
2018-06-09 01:25:03 +0900

    use export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import { Router as router } from './Router'
-import { Route as route } from './Route'
+// import { Router as router } from './Router'
+// import { Route as route } from './Route'
+//
+// export const Router = router
+// export const Route = route
 
-export const Router = router
-export const Route = route
+import { Router } from './Router'
+import { Route } from './Route'
+
+export {
+  Router,
+  Route,
+}
